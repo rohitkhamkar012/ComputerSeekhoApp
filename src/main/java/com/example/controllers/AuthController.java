6fff65e08947fa8419d018cbfe2caf456840c233
rohitkhamkar012 <120374177+rohitkhamkar012@users.noreply.github.com>
2026-01-22 13:25:33 +0530

    Update AuthController.java

--- src/main/java/com/example/controllers/AuthController.java
+++ src/main/java/com/example/controllers/AuthController.java
@@ -1,21 +1,51 @@
 package com.example.controllers;
-import java.time.LocalDateTime;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
-import com.example.DTO.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
+import com.example.entities.Staff;
+import com.example.filter.JwtUtil;
+import com.example.services.StaffService;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    
-    @PostMapping("/signIn")
-    ResponseEntity<ApiResponse> signIn() {
-        return ResponseEntity.ok(new ApiResponse("Sign In successfull", LocalDateTime.now()));
-    }
 
+    @Autowired
+    private StaffService staffService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/login")
+    public Map<String, String> login(@RequestBody Map<String, String> data) {
+
+        String username = data.get("username");
+        String password = data.get("password");
+
+        Staff staff = staffService.findByStaffUsername(username);
+
+        if (staff == null) {
+            throw new RuntimeException("invalid username or password");
+        }
+
+        boolean match = passwordEncoder.matches(
+                password,
+                staff.getStaffPassword()
+        );
+
+        if (!match) {
+            throw new RuntimeException("invalid username or password");
+        }
+
+        String token = jwtUtil.generateToken(staff.getStaffUsername());
+
+        return Map.of("token", token);
+    }
 }
